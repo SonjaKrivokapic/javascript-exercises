af402cbbbf015c5a68c7d0c4f3e2f819796b8e5f
Sonja <sonjakrivokapic00@gmail.com>
2023-05-08 15:07:26 +0200

    edit

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    let sum = 0;
+   for(let i = x;i < y; x++){
+       sum += x
+       x++
+   }
 
+    return sum
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
+//trebaju ti svi borjevi izmedju 1 i 5
+//if x < y
