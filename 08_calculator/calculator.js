0d6db48620f7acf2a25c3288e873d0b6660c15e4
Sonja <sonjakrivokapic00@gmail.com>
2023-05-09 09:06:00 +0200

    add_excersise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x + y
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let count = 0;
+  for(let i = 0; i < array.length;i ++){
+    count += array[i]
+  }
+  return count
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let count = 0;
+  for(let i = 0; i < array.length;i ++){
+    count *= array[i]
+  }
+  return count
+  // return array.reduce((total, current) => total + current, 0);
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if(n === 0){
+    return 1;
+  }
+	return n!=n*(n-1)
 };
 
 // Do not edit below this line
